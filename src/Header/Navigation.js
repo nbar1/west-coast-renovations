bb05b6eec5833fbe5c69dcfa312451a3f3089762
Nick Barone <nick@nbar1.com>
2019-11-26 23:24:08 -0500

    Add navigation active status

--- src/Header/Navigation.js
+++ src/Header/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import { Link, withRouter } from 'react-router-dom';
+import styled, { css } from 'styled-components';
 
 const NavigationWrapper = styled.div`
 	background-image: linear-gradient(to bottom, #fff, #f2f2f2);
@@ -11,37 +11,58 @@ const NavigationWrapper = styled.div`
 	text-align: center;
 `;
 
-const StyledLink = styled(Link)`
-	display: inline-block;
-	padding: 0 30px;
-	line-height: 40px;
-	height: 40px;
-	color: #777;
-	text-decoration: none;
-	text-shadow: 0 1px 0 #e2e2e2;
-	font-weight: 300;
+const StyledLink = styled.div`
 	border-left: 1px solid #f2f2f2;
 	border-right: 1px solid #fff;
+	display: inline-block;
+	font-weight: 300;
+
+	a {
+		color: #777;
+		height: 40px;
+		line-height: 40px;
+		padding: 13px 30px;
+		text-decoration: none;
+		text-shadow: 0 1px 0 #e2e2e2;
+
+		:hover {
+			color: #333;
+		}
+	}
 
 	:last-child {
 		border-right: 1px solid #f2f2f2;
 	}
 
-	:hover {
-		color: #333;
-	}
+	${props =>
+		props.active === true &&
+		css`
+			background-color: #e5e5e5;
+			box-shadow: inset 0 3px 8px rgba(0, 0, 0, 0.125);
+
+			a {
+				color: #333;
+			}
+		`}
 `;
 
-const Navigation = () => {
+const Navigation = ({ location }) => {
+	console.log(location);
 	return (
 		<NavigationWrapper>
 			<nav>
-				<StyledLink to={'/about-us'}>About Us</StyledLink>
-				<StyledLink to={'/custom-renovations'}>Custom Renovations</StyledLink>
-				<StyledLink to={'/services'}>Services</StyledLink>
+				<StyledLink active={location.pathname === '/about-us'}>
+					<Link to={'/about-us'}>About Us</Link>
+				</StyledLink>
+				<StyledLink active={location.pathname === '/custom-renovations'}>
+					<Link to={'/custom-renovations'}>Custom Renovations</Link>
+				</StyledLink>
+				<StyledLink active={location.pathname === '/services'}>
+					<Link to={'/services'}>Services</Link>
+				</StyledLink>
 			</nav>
 		</NavigationWrapper>
 	);
 };
 
-export default Navigation;
+export default withRouter(Navigation);
